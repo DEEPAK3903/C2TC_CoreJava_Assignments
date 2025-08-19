9c74af406f7a053418deccc8a9c766ac3c33cee5
Senthil Nathan.R <Senthil Nathan.R@LAPTOP-GGNJHGC9>
2025-08-19 15:40:46 +0530

    assignment1

---
+++ C2TC_Assignments/src/assignment1/employees/Employee.java
@@ -0,0 +1,42 @@
+package assignment1.employees;
+
+public class Employee {
+	private String name;
+    private int employeeId;
+    private double salary;
+
+    // Constructor
+    public Employee(String name, int employeeId, double salary) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.salary = salary;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+    protected void setSalary(double salary) { // Protected: accessible in subclasses
+        this.salary = salary;
+    }
+
+    // Display method
+    public void displayInfo() {
+        System.out.println("Name: " + name + ", ID: " + employeeId + ", Salary: " + salary);
+    }
+}
+}
